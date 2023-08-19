f6cf28081509b8c45e88ad475fb9272888f29518
rmetzger <rmetzger@dontpaniclabs.com>
2023-08-19 13:46:11 -0500

    Flesh out basic tests

--- WebApplicationFactoryEx.Test/API/BasicTests.cs
+++ WebApplicationFactoryEx.Test/API/BasicTests.cs
@@ -1,6 +1,24 @@
-namespace WebApplicationFactoryEx.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
 
-public class BasicTests
+namespace WebApplicationFactoryEx.API
 {
-    
+
+    public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        
+        public BasicTests(WebApplicationFactory<Program> factory) {  _factory = factory; }
+        
+        [Fact]
+        public async void HappyPath()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/WeatherForecast");
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
 }
