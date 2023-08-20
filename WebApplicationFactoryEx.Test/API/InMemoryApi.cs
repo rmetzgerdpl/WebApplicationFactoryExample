8c9115f68938ed3882a023dccf246a6cba6236cb
rmetzger <rmetzger@dontpaniclabs.com>
2023-08-20 17:42:33 -0500

    Add in memory api

---
+++ WebApplicationFactoryEx.Test/API/InMemoryApi.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
+using WebApplicationFactoryEx.ConsoleApp.DataStore;
+
+namespace WebApplicationFactoryEx.API
+{
+    public class InMemoryApi : IDisposable
+    {
+        private WebApplicationFactory<Program> appFactory;
+
+        public InMemoryApi()
+        {
+            //appFactory = new WebApplicationFactory<NCode.Program>();
+        }
+
+        public HttpClient CreateAnonymousClient()
+        {
+            appFactory = ConfigureAppFactory();
+            return appFactory.CreateClient();
+        }
+
+        public HttpClient CreateAuthenticatedClient()
+        {
+            return ConfigureAppFactory().CreateDefaultClient(new ClientAuthenticationHandler());
+        }
+
+        private WebApplicationFactory<Program> ConfigureAppFactory(string testsettingsFile = "appsettings.test.json")
+        {
+            return new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder => 
+                {
+                    builder.ConfigureAppConfiguration( (hostContext, config) =>
+                    {
+                        config.SetBasePath(Directory.GetCurrentDirectory())
+                            .AddJsonFile(testsettingsFile);
+                    });
+
+                    builder.ConfigureServices(services =>
+                    {
+                        //Will remove existing DB Context
+                        services.AddDbContext<DbContext>(options =>
+                        {
+                            //From Microsoft.EntityFrameworkCore.InMemory library
+                            options.UseInMemoryDatabase("FileName");
+                            // Don't raise error about InMemory not supporting transactions
+                            options.ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+
+                        });
+
+
+                        //Add any other DI stuff here!
+                        services.AddScoped<IDataStore, WeatherDataStoreInMemory>();
+                    });
+                });
+        }
+
+        public void Dispose()
+        {
+            if (appFactory != null) appFactory.Dispose();
+        }
+    }
+    
+    public class ClientAuthenticationHandler : DelegatingHandler
+    {
+        private readonly IConfidentialClientApplication tokenClient;
+        private readonly IEnumerable<string> scopes;
+
+        public ClientAuthenticationHandler()
+        {
+            var appId = "SomeId";
+            var secret = "Shhhhh";
+            tokenClient = ConfidentialClientApplicationBuilder.Create(appId)
+                .WithClientSecret(secret)
+                .WithAuthority(AzureCloudInstance.AzurePublic, "tenant")
+                .Build();
+
+            scopes = new[] { $"api://{appId}/.default" };
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancel)
+        {
+            var tokenResponse = await tokenClient.AcquireTokenForClient(scopes).ExecuteAsync();
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", tokenResponse.AccessToken);
+            return await base.SendAsync(request, cancel);
+        }
+
+    }
+}
