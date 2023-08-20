964c1b88134baf2e34d43bcb602868f3a3458fa3
rmetzger <rmetzger@dontpaniclabs.com>
2023-08-20 17:32:04 -0500

    Flesh out index test

--- WebApplicationFactoryEx.Test/API/IndexPageTests.cs
+++ WebApplicationFactoryEx.Test/API/IndexPageTests.cs
@@ -1,6 +1,25 @@
-namespace WebApplicationFactoryEx.API;
+using Xunit;
 
-public class IndexPageTests
+namespace WebApplicationFactoryEx.API
 {
-    
+    public class IndexPageTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<Program> _factory;
+
+        public IndexPageTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async void HappyPath()
+        {
+            var response = await _client.GetAsync("/WeatherForecast");
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
 }
+
